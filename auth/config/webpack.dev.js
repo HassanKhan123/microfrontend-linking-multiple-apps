6d1c7415d367f9fb10b57db13820b35d9ea403f2
HassanKhan123 <hk2602809@gmail.com>
2021-06-13 16:33:38 +0500

    auth project setup

---
+++ auth/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpakPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
+      },
+      shared: packageJSON.dependencies,
+    }),
+    new HtmlWebpakPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
